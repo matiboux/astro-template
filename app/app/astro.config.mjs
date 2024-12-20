3e3ba07e40d1f7bcb42cebc74d7f2e4d9bd63453
Matiboux <matiboux@gmail.com>
2024-12-20 04:47:20 +0100

    Test i18n functions

--- app/app/astro.config.mjs
+++ app/app/astro.config.mjs
@@ -5,8 +5,10 @@ import { i18n } from '/src/config'
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://example.com',
 	i18n: i18n,
 	integrations: [
 		tailwind(),
 	],
+	trailingSlash: 'never',
 })
