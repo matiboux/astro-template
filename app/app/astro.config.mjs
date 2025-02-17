fa0a91cbf56687950bdece64fa951b49c131b72b
Matiboux <matiboux@gmail.com>
2025-02-17 01:08:30 +0100

    Install tailwind vite plugin

--- app/app/astro.config.mjs
+++ app/app/astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
+import tailwindcss from '@tailwindcss/vite'
 
 import { i18n } from '/src/config'
 
 // https://astro.build/config
 export default defineConfig({
 	i18n: i18n,
-	integrations: [
-		tailwind(),
-	],
+	vite: {
+		plugins: [
+			tailwindcss(),
+		],
+	},
 })
