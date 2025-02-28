2a4852d9c31e5a0e1965fe9432d6064dcd72d514
Matiboux <matiboux@gmail.com>
2025-03-01 00:54:39 +0100

    Define env vars in astro config

--- app/app/astro.config.mjs
+++ app/app/astro.config.mjs
@@ -1,14 +1,21 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig, envField } from 'astro/config'
 import tailwindcss from '@tailwindcss/vite'
 
 import { i18n } from '/src/config'
 
 // https://astro.build/config
 export default defineConfig({
-	i18n: i18n,
 	vite: {
 		plugins: [
 			tailwindcss(),
 		],
+	},
+	i18n: i18n,
+	env: {
+		schema: {
+			GITHUB_REPOSITORY_URL: envField.string({ context: 'client', access: 'public', optional: true }),
+			GITHUB_SHA: envField.string({ context: 'client', access: 'public', optional: true }),
+		},
+		validateSecrets: true,
 	},
 })
