8e80f118302a4a88a1b8178df8f46affc2680748
Mati <matiboux@gmail.com>
2025-03-02 13:07:42 +0100

    Update packages & build config (#13)

--- app/app/astro.config.mjs
+++ app/app/astro.config.mjs
@@ -1,12 +1,22 @@
-import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
+import { defineConfig, envField } from 'astro/config'
+import tailwindcss from '@tailwindcss/vite'
 
 import { i18n } from '/src/config'
 
 // https://astro.build/config
 export default defineConfig({
+	integrations: [],
+	vite: {
+		plugins: [
+			tailwindcss(),
+		],
+	},
 	i18n: i18n,
-	integrations: [
-		tailwind(),
-	],
+	env: {
+		schema: {
+			GITHUB_REPOSITORY_URL: envField.string({ context: 'client', access: 'public', optional: true }),
+			GITHUB_SHA: envField.string({ context: 'client', access: 'public', optional: true }),
+		},
+		validateSecrets: true,
+	},
 })
