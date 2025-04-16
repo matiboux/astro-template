94a4b41be45d0208fb1ecbf51ed292de6ac2e26c
Mati <matiboux@gmail.com>
2025-04-16 14:58:07 +0200

    Update packages, site & docker config (#25)

--- app/app/src/site.ts
+++ app/app/src/site.ts
@@ -1,3 +1,5 @@
+import { GITHUB_SHA, VERSION_TAG } from 'astro:env/client'
+
 import type { Props as BaseProps } from '~/layouts/Base.astro'
 
 export interface Site
@@ -5,6 +7,7 @@ export interface Site
 	lang?: BaseProps['lang']
 	title?: BaseProps['title']
 	description?: BaseProps['description']
+	version?: BaseProps['version']
 	author?: BaseProps['author']
 	keywords?: BaseProps['keywords']
 	generator?: BaseProps['generator']
@@ -26,6 +29,7 @@ export const site: Site = {
 		'en': 'Template project for an Astro web application',
 		'fr': 'Modèle de projet pour une application web Astro',
 	},
+	version: GITHUB_SHA || VERSION_TAG || 'dev',
 	author: 'Matiboux',
 	themeColor: '#ffffff',
 	viewportScale: 1,
