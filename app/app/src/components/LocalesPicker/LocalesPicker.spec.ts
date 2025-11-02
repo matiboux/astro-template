9bc3a8d5bff3d23fa35a5202ac2bc77121d963ba
Mati <matiboux@gmail.com>
2025-11-02 02:35:07 +0100

    Update URLs in locale picker (#77)

--- app/app/src/components/LocalesPicker/LocalesPicker.spec.ts
+++ app/app/src/components/LocalesPicker/LocalesPicker.spec.ts
@@ -2,6 +2,8 @@ import { experimental_AstroContainer as AstroContainer } from 'astro/container'
 import { expect, test } from 'vitest'
 import LocalesPicker from './LocalesPicker.astro'
 
+import { i18n as i18nConfig } from '~/config'
+
 test('LocalesPicker with defaults', async () =>
 {
 	const container = await AstroContainer.create()
@@ -9,6 +11,31 @@ test('LocalesPicker with defaults', async () =>
 
 	expect(result).toBeTruthy()
 	expect(result).not.toContain('active')
+})
+
+test('LocalesPicker with EN language', async () =>
+{
+	const container = await AstroContainer.create()
+	const result = await container.renderToString(LocalesPicker, {
+		props: {
+			lang: 'en',
+		}
+	})
+
+	expect(result).toBeTruthy()
+	expect(result).toContain('active')
+
+	if (
+		i18nConfig.routing.prefixDefaultLocale !== false
+		|| (i18nConfig.defaultLocale as string) !== 'en'
+	)
+	{
+		expect(result).toMatch(/<a href="\/en\/"[^>]+class="active"[^>]*>\s*<span class="icon icon-\[twemoji--flag-united-kingdom][^"]*"[^>]*><\/span>/)
+	}
+	else
+	{
+		expect(result).toMatch(/<a href="\/"[^>]+class="active"[^>]*>\s*<span class="icon icon-\[twemoji--flag-united-kingdom][^"]*"[^>]*><\/span>/)
+	}
 })
 
 test('LocalesPicker with FR language', async () =>
@@ -22,5 +49,16 @@ test('LocalesPicker with FR language', async () =>
 
 	expect(result).toBeTruthy()
 	expect(result).toContain('active')
-	expect(result).toMatch(/<a href="\/fr"[^>]+class="active"[^>]*>/)
+
+	if (
+		i18nConfig.routing.prefixDefaultLocale !== false
+		|| (i18nConfig.defaultLocale as string) !== 'fr'
+	)
+	{
+		expect(result).toMatch(/<a href="\/fr\/"[^>]+class="active"[^>]*>\s*<span class="icon icon-\[twemoji--flag-france][^"]*"[^>]*><\/span>/)
+	}
+	else
+	{
+		expect(result).toMatch(/<a href="\/"[^>]+class="active"[^>]*>\s*<span class="icon icon-\[twemoji--flag-france][^"]*"[^>]*><\/span>/)
+	}
 })
