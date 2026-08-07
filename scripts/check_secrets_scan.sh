9c3a4ff3036dd3f066fae2bdbaa28e309b4cf51f
Matiboux <matiboux@gmail.com>
2026-08-07 23:23:06 +0000

    Update docker run for gitleaks update-baseline

--- scripts/check_secrets_scan.sh
+++ scripts/check_secrets_scan.sh
@@ -95,18 +95,19 @@ elif command -v docker >/dev/null 2>&1; then
 					docker create -i --entrypoint sh "${docker_image}" \
 						-c "mkdir -p /repo && tar -xf - -C /repo && gitleaks git /repo --no-banner ${container_args}"
 				)"
-				if [ -z "${container_id}" ]; then
-					return 2
-				fi
+				[ -z "${container_id}" ] && return 2
 				tar -cf - -C "${repo_dir}" . | docker start -ai "${container_id}"
 				local gitleaks_rc=$?
 				local gitleaks_baseline="$(mktemp)"
-				if docker cp "${container_id}:/repo/${baseline_path}" "${gitleaks_baseline}" > /dev/null 2>&1; then
-					mv "${gitleaks_baseline}" "${repo_dir}/${baseline_path}"
+				[ "${gitleaks_rc}" -ge 2 ] && return "${gitleaks_rc}"
+				docker cp "${container_id}:/repo/${baseline_path}" "${gitleaks_baseline}" > /dev/null 2>&1
+				if [ $? -eq 0 ]; then
+					mv "${gitleaks_baseline}" "${repo_dir}/${baseline_path}" > /dev/null 2>&1
 				else
-					rm -f "${gitleaks_baseline}"
+					rm -f "${gitleaks_baseline}" > /dev/null 2>&1
+					gitleaks_rc=2
 				fi
-				docker rm "${container_id}" > /dev/null 2>&1
+				docker rm -f "${container_id}" > /dev/null 2>&1
 				return "${gitleaks_rc}"
 			}
 		else
